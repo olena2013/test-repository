ab91589955ef0686bd4f0f6b0ad54c59d113c4c1
Elena_T <ltunik@mail.ru>
2017-09-21 20:56:52 -0700

    Changes after homework

--- src/test/java/Login_into_app.java
+++ src/test/java/Login_into_app.java
@@ -1,12 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import java.util.concurrent.TimeUnit;
 
 public class Login_into_app {
+    @Test
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lena\\Documents\\GitHub\\test-repository\\driver_chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
 
         driver.get("http://localhost/litecart/public_html/admin/login.php");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 
 import java.util.concurrent.TimeUnit;
 
+@Test
 public class MyFirstTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lena\\Documents\\GitHub\\test-repository\\driver_chrome\\chromedriver.exe");
@@ -12,7 +17,6 @@ public class MyFirstTest {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         driver.manage().window().maximize();
-
         driver.quit();
 
 
